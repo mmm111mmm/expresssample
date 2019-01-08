315dff48d7da28f5c89836f9e3f88558319ddd94
Aaron Newell <user@Aarons-MacBook-Air.local>
2019-01-08 16:03:30 +0100

    merged

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(PORT);
+  console.log('Express started on port ' + PORT);
+}
